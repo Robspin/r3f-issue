bdb482064287e4f1c527f4f694ca82f4f61f103a
Robin Steeman <robin@tracefy.com>
2023-05-23 11:51:49 +0900

    simplified code

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,15 @@
 import * as THREE from 'three'
 import { forwardRef, useEffect, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import {useGLTF, ScrollControls, useScroll, useTexture, Preload} from '@react-three/drei'
+import { useGLTF, ScrollControls, useScroll, useTexture } from '@react-three/drei'
 import useRefs from 'react-use-refs'
-
-const rsqw = (t, delta = 0.1, a = 1, f = 1 / (2 * Math.PI)) => (a / Math.atan(1 / delta)) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
-
-import dynamic from 'next/dynamic'
-import {r3f} from "@/helpers/global"
 
 export default function App() {
   return (
       <Canvas shadows dpr={[1, 2]} camera={{ position: [0, -3.2, 40], fov: 12 }}>
-        <r3f.Out />
         <ScrollControls pages={5}>
           <Composition />
         </ScrollControls>
-        <Preload all />
       </Canvas>
   )
 }
@@ -36,10 +29,6 @@ function Composition({ ...props }) {
     const r3 = scroll.visible(4 / 5, 1 / 5)
 
     mbp16.current.rotation.x = Math.PI - (Math.PI / 2) * 0.96 + r2 * 0.33
-    // mbp16.current.rotation.x = Math.PI - (Math.PI / 2) * rsqw(r1) + r2 * 0.33
-    // group.current.rotation.y = THREE.MathUtils.damp(group.current.rotation.y, (-Math.PI / 1.45) * r2, 4, delta)
-    group.current.position.x = THREE.MathUtils.damp(group.current.position.x, (-width / 7) * r2, 4, delta)
-    group.current.scale.x = group.current.scale.y = group.current.scale.z = THREE.MathUtils.damp(group.current.scale.z, 1 + 0.24 * (1 - rsqw(r1)), 4, delta)
     keyLight.current.position.set(0.25 + -15 * (1 - r1), 4 + 11 * (1 - r1), 3 + 2 * (1 - r1))
   })
 
